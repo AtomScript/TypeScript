f9ba63e8516b29a76962e024056ceb1c056b76a5
Rafael <77973453+AtomScript@users.noreply.github.com>
2021-04-26 13:50:17 -0400

    Update parser.ts

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -44,6 +44,7 @@ namespace ts {
             if (cbNodes) {
                 return cbNodes(nodes);
             }
+
             for (const node of nodes) {
                 const result = cbNode(node);
                 if (result) {
